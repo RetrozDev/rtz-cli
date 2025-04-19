2a4b0dd449154cea296465e7f7737bbbd8fafa4b
RetrozDev <ryanbarbozapro@gmail.com>
2025-04-19 22:32:27 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+const program = new Command();
+
+program
+	.name('rtz')
+	.description('An awesome CLI for your workflow')
+	.version('1.0.0');
+
+program
+	.command('hello')
+	.description('Show a hello message')
+	.option('-n, --name <name>', 'Name to greet', 'Retroz CLI')
+	.action((options) => {
+		console.info(
+			`Hello from rtz-cli! ${chalk.blue('Welcome to the CLI!')} ${chalk.green(options.name)}`,
+		);
+	});
+
+program.parse(process.argv);
