afb31458c282c9dc61d4d05fae9d7eaff504d314
RetrozDev <ryanbarbozapro@gmail.com>
2025-04-20 00:05:21 +0200

    Added version check

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,13 @@ import { Command } from 'commander';
 import { commands } from './commands/commandsList.js';
 import { helpCommand } from './commands/actions/help.js';
 import { CommandHandler } from './handlers/CommandHandler.js';
+import packageJson from '../package.json' with { type: 'json' };
+const version = packageJson.version;
 const program = new Command();
 program
     .name('rtz')
     .description('An awesome CLI for your workflow')
-    .version('1.0.0');
+    .version(version, '-v, --version', 'output the current version');
 const commandHandler = new CommandHandler(program);
 // Register all commands
 for (const cmd of commands) {
